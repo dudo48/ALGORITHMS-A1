a0b4e45f444251c2bc790fc1d2b98eb74f0672e4
moaz basheer <moazbasheer97@gmail.com>
2020-12-27 18:52:07 +0200

    update deletion

--- RBTree-GUI/src/RBTree.java
+++ RBTree-GUI/src/RBTree.java
@@ -1,5 +1,3 @@
-import javax.swing.JOptionPane;
-
 public class RBTree {
     Node root;
     int node_count;
@@ -8,48 +6,7 @@ public class RBTree {
         node_count = 0;
     }
     
-    public void leftRotationinsert(Node node) {
-        Node temp= node.right;// make a temporary node hold the right node .
-        node.right= temp.left;// make the left child of the right node equals the left child of the right node---> the node that will go up .
-        if(temp.left != null)
-            temp.left.parent=node;// announce the left child with his new father .
-        
-        temp.parent=node.parent;//replace the parent of the left node with that of the right node .
-        
-        if(node.parent==null)//node is the root .
-            this.root=temp;
-        
-        else if (node.parent.left==node)//check the location of the node (according to the father) to replace the other node in the same place as a left or right child of the parent .
-                node.parent.left=temp;
-        
-        else node.parent.right=temp;
-        
-        temp.left=node;
-        node.parent=temp;
-        
-        
-    }
-    
-    public void rightRotaioninsert(Node node) {
-        Node temp= node.left;
-        node.left=temp.right;
-        if(temp.right!=null)
-            temp.right.parent=node;
-        
-        temp.parent=node.parent;
-        if(node.parent==null)
-            this.root=temp;
-        
-        else if (node.parent.right==node)
-            node.parent.right=temp;
-        else node.parent.left= temp;
-        
-        temp.right=node;
-        node.parent=temp;
-        
-        
-        
-    }
+
     private void fixInsert(Node node) {
         
         Node uncle=null ;//referes to uncle .
@@ -68,12 +25,12 @@ public class RBTree {
                     {
                         //right left case :1)right rotate parent to convert it to right right case .
                         node=node.parent;
-                        rightRotaioninsert(node);
+                        rightRotation(node);
                     }
                     //2)then left rotate grand parent and swap their colors . [right right case ]
                     node.parent.color=false;
                     node.parent.parent.color=true ;
-                    leftRotationinsert(node.parent.parent);
+                    leftRotation(node.parent.parent);
                     
                 }
             }else {// the parent is the left child of the grand parent .
@@ -86,13 +43,13 @@ public class RBTree {
                 else {
                     if (node==node.parent.right) { // left right case ;
                     node=node.parent;   
-                    leftRotationinsert(node);
+                    leftRotation(node);
                     }
                     
                     // left left case ;
                     node.parent.color=false;
                     node.parent.parent.color=true;
-                    rightRotaioninsert(node.parent.parent);
+                    rightRotation(node.parent.parent);
                 }
                 
             }
@@ -101,7 +58,7 @@ public class RBTree {
         }
         this.root.color=false;
     }
-
+    
     public void insertNode(int key) {
         Node newNode= new Node(key);// saves the key and make the color red.
         newNode.parent=null;
@@ -159,10 +116,12 @@ public class RBTree {
     public void prettyPrint() {
         printHelper(this.root, "", true);
     }
+    
     public void clear() {
         root = null;
         node_count = 0;
     }
+    
     Node getSuccessor(Node node) {
         node = node.getRight();
         while(node.getLeft() != null) {
@@ -178,7 +137,6 @@ public class RBTree {
     public Node searchNode(Node node, int key) {
         if(node == null) 
             return null;
-        System.out.println(node.getKey());
         if(node.getKey() < key) {
             return searchNode(node.getRight(), key);
         } else if(node.getKey() > key) {
@@ -189,14 +147,15 @@ public class RBTree {
     }
 
     public void deleteKey(int key) {
-        this.root = deleteKey(root, key);
+        deleteKey(root, key);
     }
-    
+
     public void leftRotation(Node node) {
-        Node temp= node.getRight();// make a temporary node hold the right node .
+        
+        Node temp = node.getRight();// make a temporary node hold the right node .
         node.setRight(temp.getLeft());// make the left child of the right node equals the left child of the right node---> the node that will go up .
-        if(temp.getRight() != null)
-            temp.getRight().setParent(node);// announce the left child with his new father .
+        if(node.getRight() != null)
+            node.getRight().setParent(node);// announce the left child with his new father .
         
         temp.setParent(node.getParent());//replace the parent of the left node with that of the right node .
         
@@ -207,21 +166,19 @@ public class RBTree {
         
         else node.getParent().setRight(temp);
         
-        node.setParent(temp);
         temp.setLeft(node);
-        
+        node.setParent(temp);
     }
     
     public void rightRotation(Node node) {
-        Node temp= node.getLeft();
+        Node temp = node.getLeft();
         node.setLeft(temp.getRight());
-        if(temp.getLeft()!=null)
-            temp.getLeft().setParent(node);
+        if(node.getLeft() != null)
+            node.getLeft().setParent(node);
         
         temp.setParent(node.getParent());
-        if(node.getParent()==null)
-            this.root=temp;
-        
+        if(node.getParent() == null)
+            this.root = temp;
         else if (node.getParent().getLeft() == node)
             node.getParent().setLeft(temp);
         else node.getParent().setRight(temp);
@@ -229,132 +186,160 @@ public class RBTree {
         node.setParent(temp);
         
     }
+    public void traverse(Node node) {
+        if(node == null)
+            return ;
+        
+        traverse(node.getLeft());
+        traverse(node.getRight());
+    }
     
-    public Node deleteKey(Node node, int key) {
+    public void deleteKey(Node node, int key) {
         if(node == null) {
             System.out.println("The key is not found");
-            return null;
+            return;
         }
         if(node.getKey() < key) {
-            Node x = deleteKey(node.getRight(), key);
-            node.setRight(x);
-            if(x != null)
-                x.setParent(node);
+            deleteKey(node.getRight(), key);
         } else if(node.getKey() > key) {
-            Node x = deleteKey(node.getLeft(), key);
-            node.setLeft(x);
-            if(x != null)
-                x.setParent(node);
+            deleteKey(node.getLeft(), key);
         } else {
             if(node.getLeft() == null && node.getRight() == null) {
                 adjustDelete(node);
-                --node_count;
-                return null;
+                if(node == root) {
+                    this.root = null;
+                } else if(node == node.getParent().getLeft()) {
+                    node.getParent().setLeft(null);
+                    node.setParent(null);
+                } else {
+                    node.getParent().setRight(null);
+                    node.setParent(null);
+                }
+                return ;
             } else if(node.getLeft() == null) {
                 adjustDelete(node);
-                --node_count;
-                return node.getRight();
+                if(node == root) {
+                    this.root = node.getRight();
+                    node.getRight().setParent(null);
+                } else if(node == node.getParent().getLeft()) {
+                    node.getParent().setLeft(node.getRight());
+                    node.getRight().setParent(node.getParent());
+                } else {
+                    node.getParent().setRight(node.getRight());
+                    node.getRight().setParent(node.getParent());
+                }
+                node.setParent(null);
+                return ;
             } else if(node.getRight() == null) {
                 adjustDelete(node);
-                --node_count;
-                return node.getLeft();
+                if(node == root) {
+                    this.root = node.getLeft();
+                    node.getLeft().setParent(null);
+                } else if(node == node.getParent().getLeft()) {
+                    node.getParent().setLeft(node.getLeft());
+                    node.getLeft().setParent(node.getParent());
+                } else {
+                    node.getParent().setRight(node.getLeft());
+                    node.getLeft().setParent(node.getParent());
+                }
+                node.setParent(null);
+                return ;
             }
             // swapping
             Node suc = getSuccessor(node);
             int nodeKey = node.getKey();
             node.setKey(suc.getKey());
             suc.setKey(nodeKey);
-            // continue traversing
-            Node x = deleteKey(node.getRight(), key);
-            node.setRight(x);
-            if(x != null)
-                x.setParent(node);
+            
+            deleteKey(node.getRight(), key);
         }
-        return node;
     }
 
     private void adjustDelete(Node node) {
-        if(node == null || node.isRed() || node.getParent() == null) 
+        
+        if(node == null || node.isRed())  // if it is red, do nothing.
             return ;
+        // if it has a red child, make it black.
         if(node.getRight() != null && node.getRight().isRed()) 
             node.getRight().makeBlack();
         else if(node.getLeft() != null && node.getLeft().isRed())
             node.getLeft().makeBlack();
         else {
+            // if it's black and has both black children.(double-black).
             boolean isDoubleBlack = true;
             Node temp = node;
-            while(isDoubleBlack && temp.getParent() != null) {
+            while(isDoubleBlack && temp != root) {
                 if(temp == temp.getParent().getLeft()) {
-                    if(temp.getParent().getRight().isRed()) {
+                    // if the sibling is red.
+                    if(temp.getParent().getRight() != null && temp.getParent().getRight().isRed()) {
                         temp.getParent().getRight().makeBlack();
                         temp.getParent().makeRed();
-                        this.rightRotation(temp.getParent());
+                        this.leftRotation(temp.getParent());
                     } else {
-                        if((temp.getParent().getRight().getLeft() == null 
-                                || !temp.getParent().getRight().getLeft().isRed() ) 
-                       && (temp.getParent().getRight().getRight() == null || 
-                       !temp.getParent().getRight().getRight().isRed() )) {
+                        // if it's black
+                        if((temp.getParent().getRight().getLeft() == null ||!temp.getParent().getRight().getLeft().isRed()) 
+                       && ( temp.getParent().getRight().getRight() == null ||!temp.getParent().getRight().getRight().isRed()))                               {
                             
                             temp.getParent().getRight().makeRed();
-                            if(temp.getParent().isRed()) {
+                            if(temp.getParent() != null && temp.getParent().isRed()) {
                                 temp.getParent().makeBlack();
                                 isDoubleBlack = false;
-                            } else {
-                                temp.getParent().makeBlack();
                             }
                             temp = temp.getParent();
                         } else {
-                            if(!temp.getParent().getRight().getRight().isRed()) {
-                                
+                            
+                            if(temp.getParent().getRight().getRight() == null || 
+                                    !temp.getParent().getRight().getRight().isRed()) {
                                 temp.getParent().getRight().getLeft().makeBlack();
                                 temp.getParent().getRight().makeRed();
-                                rightRotation(temp.getParent().getRight());
-                                
+                                this.rightRotation(temp.getParent().getRight());
                             }
-                            if(temp.getParent().isRed())
+                            
+                            if(temp.getParent() != null && temp.getParent().isRed())
                                 temp.getParent().getRight().makeRed();
                             else 
                                 temp.getParent().getRight().makeBlack();
+                            
                             temp.getParent().makeBlack();
                             temp.getParent().getRight().getRight().makeBlack();
+                            
                             this.leftRotation(temp.getParent());
+                            
                             break;
                         }
                     }
                 } else {
-                    if(temp.getParent().getLeft().isRed()) {
+                    if(temp.getParent().getLeft() != null && temp.getParent().getLeft().isRed()) {
                         temp.getParent().getLeft().makeBlack();
                         temp.getParent().makeRed();
                         this.rightRotation(temp.getParent());
                     } else {
-                        if((temp.getParent().getRight().getLeft() == null ||
-                            !temp.getParent().getRight().getLeft().isRed() ) 
-                       && (temp.getParent().getRight().getRight() == null
-                           || !temp.getParent().getRight().getRight().isRed())) {
-                            
+                        if((temp.getParent().getLeft().getLeft() == null ||
+                                !temp.getParent().getLeft().getLeft().isRed() ) 
+                       && (temp.getParent().getLeft().getRight() == null
+                       || !temp.getParent().getLeft().getRight().isRed())) {
                             temp.getParent().getLeft().makeRed();
                             if(temp.getParent().isRed()) {
                                 temp.getParent().makeBlack();
                                 isDoubleBlack = false;
-                            } else {
-                                temp.getParent().makeBlack();
                             }
                             temp = temp.getParent();
                         } else {
-                            if(!temp.getParent().getLeft().getLeft().isRed()) {
+                            if(temp.getParent().getLeft().getLeft() == null || 
+                                    !temp.getParent().getLeft().getLeft().isRed()) {
                                 
                                 temp.getParent().getLeft().getRight().makeBlack();
                                 temp.getParent().getLeft().makeRed();
-                                rightRotation(temp.getParent().getLeft());
+                                this.leftRotation(temp.getParent().getLeft());
                                 
                             }
-                            if(temp.getParent().isRed())
+                            if(temp.getParent() != null && temp.getParent().isRed())
                                 temp.getParent().getLeft().makeRed();
                             else 
                                 temp.getParent().getLeft().makeBlack();
                             temp.getParent().makeBlack();
-                            temp.getParent().getRight().getLeft().makeBlack();
-                            this.leftRotation(temp.getParent());
+                            temp.getParent().getLeft().getLeft().makeBlack();
+                            this.rightRotation(temp.getParent());
                             break;
                         }
                     }
@@ -362,6 +347,5 @@ public class RBTree {
             }
             this.root.makeBlack();
         }
-        
     }
 }
